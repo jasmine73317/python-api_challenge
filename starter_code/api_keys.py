b2c3368b0773b3534390a9c60eb2909420cdeed6
jasmine73317 <61628880+jasmine73317@users.noreply.github.com>
2020-07-25 16:28:15 -0500

    del key

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -2,7 +2,5 @@
 weather_api_key = "ca033fb144bdbefbb8ef8453ce2bc91c"
 
 # Google API Key
-g_key = "AIzaSyB2tpD8rsNfBNlxUE0jZgH1dkRta9R61I8"
+g_key = ""
 
-#AIzaSyB2tpD8rsNfBNlxUE0jZgH1dkRta9R61I8
-#b0c509271594341dfea73022ebef82b67ec97aca
